e7aac55e21471976e0a12b4b0ca4351a0347c206
marcallen.david.18@gmail.com <marcallen.david.18@gmail.com>
2023-02-25 23:07:09 +0800

    quick fix

--- config/db_data.php
+++ config/db_data.php
@@ -1,6 +1,6 @@
 <?php
-define('HOST','localhost');
+define('HOST','127.0.0.1:3308');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'walker09');
 define('DB_NAME', 'scheduling_system_db');//change me
 ?>
